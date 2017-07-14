c83a0863dfbd7e204f300d07a477ed8e7cee61a5
Stephenmhuang <Stephenmhuang@live.com>
2017-07-14 13:45:03 -0500

    complete no build

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -15,7 +15,14 @@ public class Card : MonoBehaviour {
 	public GameObject back;     // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
-	
+
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start()
+    {
+        SetSortOrder(0);
+    }
+
 	// property
 	public bool faceUP {
 		get {
@@ -24,7 +31,58 @@ public class Card : MonoBehaviour {
 		set {
 			back.SetActive(!value);
 		}
-	}	
+	}
+
+    public void PopulateSpriteRenderers()
+    {
+        if(spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            if(tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
+
 } // class Card
 
 [System.Serializable]
